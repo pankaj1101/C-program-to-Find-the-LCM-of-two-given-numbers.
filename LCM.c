20933bd5bbbd04fbc4fa8061fbe17c345bbab2d3
Pankaj Ram <116742441+pankaj1101@users.noreply.github.com>
2022-11-01 16:00:48 +0530

    Add files via upload

---
+++ LCM.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main()
+{
+    int num1, num2;
+    int temp = 0;
+
+    printf("Enter 1st Number : ");
+    scanf("%d", &num1);
+    printf("Enter 2nd Number : ");
+    scanf("%d", &num2);
+
+    if (num1 > num2)
+    {
+        temp = num1;
+        for (int j = 2;; j++)
+        {
+            if (temp % num2 == 0)
+            {
+                printf("LCM of %d and %d is : %d", num1, num2, temp);
+                break;
+            }
+            temp = num1 * j;
+        }
+    }
+    else
+    {
+        temp = num2;
+        for (int j = 2;; j++)
+        {
+            if (temp % num1 == 0)
+            {
+                printf("LCM of %d and %d is : %d", num1, num2, temp);
+                break;
+            }
+            temp = num2 * j;
+        }
+    }
+    return 0;
+}
